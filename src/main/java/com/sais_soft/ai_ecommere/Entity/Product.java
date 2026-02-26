c86c3b32e83fc3f5dce4a0c8727f40d9fec07c00
Prahasith <vuddagiriprahasith@gmail.com>
2026-02-27 00:27:36 +0530

    create first realtionship

--- src/main/java/com/sais_soft/ai_ecommere/Entity/Product.java
+++ src/main/java/com/sais_soft/ai_ecommere/Entity/Product.java
@@ -12,6 +12,8 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -69,5 +71,10 @@ public class Product {
   
   @Column(name ="is_deleted",nullable=false)
   private boolean isDeleted;
+  
+  
+  @ManyToOne
+  @JoinColumn(name ="category_id")
+  private Category category;
   
 }
